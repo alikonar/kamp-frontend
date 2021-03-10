709bc214a1d784d2604326683c31f6ef8fc978a0
Ali Konar <ali.knr@hotmail.com>
2021-03-10 22:26:40 +0300

    Front-End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; //using kısmı gibii.
 
-@Component({
-  selector: 'app-root',
+@Component({  //HTML'in datasını yönettiğimz yer.
+  selector: 'app-root', //app-root tagini tanıyabilmesi için selector'e ekliyoruz.
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] //HTML'in şekilli şukullu olmasını sağladığmız yer.
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind'; //string ekleyerek type security yapıyoruz.
+  user: string = "Ali Konar";
+  
 }
