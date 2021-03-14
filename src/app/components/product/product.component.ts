9c2b88117b75ce8574e03d15b7657d07f9aba946
Ali Konar <ali.knr@hotmail.com>
2021-03-14 14:09:12 +0300

    New

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import { HttpClient } from '@angular/common/http'; //bir backende istekte bulunmaya yarayan import.
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+
 
 
 
@@ -11,20 +11,21 @@ import { ProductResponseModel } from 'src/app/models/productResponseModel';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  products: Product[] = [];
-  apiUrl = 'https://localhost:44383/api/products/getall';
-  constructor(private httpClient: HttpClient) { }
+  
+  products: Product[]=[];
+  dataLoaded=false;
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts() {
-    this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
-      .subscribe((response)=> {
-        this.products = response.data
-      });
+    this.productService.getProducts().subscribe(response=>{
+      this.products = response.data
+      this.dataLoaded=true;
+    })
+
   }
 
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  apiUrl='https://localhost:44383/api/products/getall'
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<ListResponseModel<Product>>{
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  }
+}
